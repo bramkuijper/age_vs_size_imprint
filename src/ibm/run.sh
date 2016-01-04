081725e9c549747cb9daa2c5d7a5644f98a76de7
Bram Kuijper <bramkuijper@gmail.com>
2016-01-04 21:42:59 +0000

    added maternal control

--- src/ibm/run.sh
+++ src/ibm/run.sh
@@ -2,5 +2,5 @@
 
 #./xage_vs_size_imprint_nf2_nm2 0.01 0.02 0.5 0.2 0.33333 1 0 2
 #./xage_vs_size_imprint_nf2_nm2 0.01 0.02 0.5 0.1 0.333333333333 1.0     0 0
-./xage_vs_size_imprint_nf1_nm4 0.01 0.02 0.5 0.1 0.333333333333 1 1 0
+./xage_vs_size_imprint_nf2_nm2 0.01 0.02 0.5 0.0 0.333333333333 1 0 0
 #./xage_vs_size_imprint_nf1_nm1 0.01 0.02 0.5 0.0 0.333333333333 0.25 0 0 
