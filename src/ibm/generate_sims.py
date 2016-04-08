622618b53ffdbebe00808556d9d2a74630c47d9f
bram kuijper <bramkuijper@pomiankowski-md00.biol.ucl.ac.uk>
2016-04-08 22:41:40 +0100

    change nonlocal mating in generate sims

--- src/ibm/generate_sims.py
+++ src/ibm/generate_sims.py
@@ -4,13 +4,14 @@ import numpy as np
 import math, sys
 
 nf_nm = [[2,2],[3,2],[2,3]]
-df = [ 0.1, 0.5, 0.9 ]
 dm = list(np.arange(0,1.05,0.05))
 q = 1
 k = 1.0/3
 
-type = [ 3, 4 ]
+type = [ 0 ]
 diploid = [ 0, 1 ] 
+
+nonlocalmating = [ 0.5, 0.75] 
 
 #exe = "./xage_vs_size_imprint"
 exe = "/home/uccoaku/age_vs_size_imprint/src/ibm/xage_vs_size_imprint"
@@ -18,15 +19,16 @@ exe = "/home/uccoaku/age_vs_size_imprint/src/ibm/xage_vs_size_imprint"
 mu = 0.01
 sdmu = 0.02
 
-reps = 3
+reps = 1
 
 ctr = 0
 
 for nfnm_i in nf_nm:
     nf_i = nfnm_i[0]
     nm_i = nfnm_i[1]
-    for df_i in df:
-        for dm_i in dm:
+    for dm_i in dm:
+        df_i = 1-dm_i
+        for nonlocal_i in nonlocalmating:
             for type_i in type:
                 for diploid_i in diploid:
                     for rep in range(0, reps):
@@ -40,6 +42,6 @@ for nfnm_i in nf_nm:
                         print(exe_i + 
                                 str(mu) + " " + str(sdmu) + " " + 
                                 str(df_i) + " " + str(dm_i) + " " +
-                                str(k) + " " + str(q) + " " +
+                                str(k) + " " + str(q) + " " + str(nonlocal_i) + " " +
                                 str(diploid_i) + " " + str(type_i))
 
