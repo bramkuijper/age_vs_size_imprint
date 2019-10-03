13de6c15f9c67cc4003137e762d7c421aefcfa62
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2019-10-03 21:29:52 +0100

    ok adding the contour plots

--- figs/effect_phil/generate_battleground_data.r
+++ figs/effect_phil/generate_battleground_data.r
@@ -84,40 +84,50 @@ get_sols <- function(nf, nm, l, k, μ, df, dm) {
 
 dfvals <- seq(0.01,0.99,0.01)
 
+nfvals <- seq(1,7.05,0.05)
+
 lvals <- c(0.1,0.5, 0.9, 1.0)
 
 the.df <- NULL
+
+ctr <- 1
 
 for (l.i in lvals)
 {
     for (df.i in dfvals)
     {
-        temp.df <- get_sols(nf=2, nm=2, l=l.i, k=1.0/3, μ=1.0, df=df.i, dm=1.0 - df.i)
+        for (nf.i in nfvals)
+        {
+            print(ctr)
+            temp.df <- get_sols(nf=nf.i, nm=8-nf.i, l=l.i, k=1.0/3, μ=1.0, df=df.i, dm=1.0 - df.i)
 
-        the.df <- rbind(the.df, temp.df)
+            the.df <- rbind(the.df, temp.df)
+
+            ctr <- ctr + 1
+        }
     }
 }
 
-write.table(x=the.df,file="battleground_data_vary_df.csv",sep=";",row.names=F)
+write.table(x=the.df,file="battleground_data_vary_df_nf.csv",sep=";",row.names=F)
 
-print("df done.")
-
-the.df <- NULL
-
-nfvals <- seq(1.0, 8.0, 0.05)
-
-lvals <- c(0.1,0.5, 0.9, 0.99)
-
-for (l.i in lvals)
-{
-    for (nf.i in nfvals)
-    {
-        print(nf.i)
-        print(max(nfvals) + 1 - nf.i)
-        temp.df <- get_sols(nf=nf.i, nm=max(nfvals) + 1 - nf.i, l=l.i, k=1.0/3, μ=1.0, df=0.5, dm=0.5)
-        
-        the.df <- rbind(the.df, temp.df)
-    }
-}
-
-write.table(x=the.df,file="battleground_data_vary_nf.csv",sep=";",row.names=F)
+#print("df done.")
+#
+#the.df <- NULL
+#
+#nfvals <- seq(1.0, 8.0, 0.05)
+#
+#lvals <- c(0.1,0.5, 0.9, 0.99)
+#
+#for (l.i in lvals)
+#{
+#    for (nf.i in nfvals)
+#    {
+#        print(nf.i)
+#        print(max(nfvals) + 1 - nf.i)
+#        temp.df <- get_sols(nf=nf.i, nm=max(nfvals) + 1 - nf.i, l=l.i, k=1.0/3, μ=1.0, df=0.5, dm=0.5)
+#        
+#        the.df <- rbind(the.df, temp.df)
+#    }
+#}
+#
+#write.table(x=the.df,file="battleground_data_vary_nf.csv",sep=";",row.names=F)
---
+++ figs/effect_phil/overview_conflict_contour.r
@@ -0,0 +1,79 @@
+library("lattice")
+library("magrittr")
+library("tidyr")
+library("dplyr")
+library("readr")
+library("colorRamps")
+library("RColorBrewer")
+
+# plot conflicts out in a contour plot
+
+if (!exists("the.data.long"))
+{
+    the.data <- read.table("battleground_data_vary_df_nf.csv"
+            ,sep=";"
+            ,header=T)
+
+    the.data <- the.data %>% subset(l %in% c(1.0,0.5))
+
+    # change into long form
+    the.data.long <- the.data %>% pivot_wider(
+            names_from = c("expression")
+            ,values_from = c("af","am"))
+}
+
+#at.range <- c(-100,seq(-0.5,0.5,0.02),100)
+
+#at.range <- c(seq(0,0.5,0.005),100)
+
+at.range <- c(0.05, 0.2, 0.5)
+
+#col.reg <- colorRampPalette(brewer.pal(8,"Dark2"))(length(table.data))
+#rwb <- colorRampPalette(colors = c("red", "white", "blue"))
+
+col.reg <- c("white","grey80","grey70","grey50","grey40")
+
+the.strip <- function(strip.levels,...) { strip.default(strip.levels=T,...) }
+
+ylim <- c(1,6.9)
+
+pdf("levelplot_battleground_AF_mi_vs_pi.pdf")
+print(
+        contourplot(
+                abs(af_PI - af_MI) ~ df * nf | genetic_system * l
+                ,data=the.data.long
+                ,at=at.range
+                ,strip=the.strip
+                ,ylim=ylim
+                ,label=F
+                ,col.regions=col.reg
+                )
+        )
+dev.off()
+
+pdf("levelplot_battleground_AM_mi_vs_pi.pdf")
+print(
+        contourplot(
+                abs(am_PI - am_MI) ~ df * nf | genetic_system * l
+                ,data=the.data.long
+                ,at=at.range
+                ,strip=the.strip
+                ,ylim=ylim
+                ,label=F
+                ,col.regions=col.reg
+                )
+        )
+dev.off()
+                
+pdf("levelplot_battleground_AF_mother_offspring.pdf")
+print(
+        levelplot(
+                abs(af_ - af_M) ~ df * nf | genetic_system * l
+                ,data=the.data.long
+                ,at=at.range
+                ,strip=the.strip
+                ,ylim=ylim
+                ,col.regions=col.reg
+                )
+        )
+dev.off()
